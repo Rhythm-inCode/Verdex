f0ba3158a330c3daf4bf69a4a807d18bda9dc926
rhythm <rhythmln1104@gmail.com>
2026-03-06 03:15:52 +0530

    fix api base url for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true
 });
 
